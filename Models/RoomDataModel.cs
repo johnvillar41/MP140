725eaa950f8268a54dc70e8bd98f053bd996d69c
johnvillar41 <johndanielvillar123@gmail.com>
2021-06-05 20:55:04 +0800

    Added RoomDataModel

---
+++ Models/RoomDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MP140.Models
+{
+    public class RoomDataModel
+    {
+        public int Id { get; set; }
+        public UserModel User { get; set; }
+        public RoomModel Room { get; set; }
+        public TodoModel TodoItem { get; set; }
+    }
+}
