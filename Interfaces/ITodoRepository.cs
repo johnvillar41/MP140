b0ae7bca14342c2a6d81a382414c1f9615c5a530
johnvillar41 <johndanielvillar123@gmail.com>
2021-06-06 21:18:50 +0800

    Added TodoLayer

---
+++ Interfaces/ITodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using MP140.Models;
+using System.Collections.Generic;
+
+namespace MP140.Interfaces
+{
+    public interface ITodoRepository
+    {
+        void AddTodoItem(TodoModel newTodo);
+        void DeleteTodoItem(int todoId);
+        List<TodoModel> FetchAllTodos();
+    }
+}
---
+++ Repositories/TodoRepository.cs
@@ -0,0 +1,37 @@
+﻿using MP140.Interfaces;
+using MP140.Models;
+using System.Collections.Generic;
+
+namespace MP140.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private static TodoRepository instance = null;
+        private TodoRepository() { }
+        public static TodoRepository SingleInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TodoRepository();
+                }
+                return instance;
+            }
+        }
+        public void AddTodoItem(TodoModel newTodo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTodoItem(int todoId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<TodoModel> FetchAllTodos()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
