4dda179a600e7931efaad4eeb8a074b8459a732a
johnvillar41 <johndanielvillar123@gmail.com>
2021-06-06 08:47:44 +0800

    Added Room Adapter, Updated Room Model, Added static image files

--- Adapter/RoomAdapter.cs
+++ Adapter/RoomAdapter.cs
@@ -1,7 +1,52 @@
-﻿namespace MP140.Adapter
-{
-    public class RoomAdapter
-    {
+﻿using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using MP140.Models;
+using System.Collections.Generic;
 
+namespace MP140.Adapter
+{
+    public class RoomAdapter : RecyclerView.Adapter
+    {
+        private List<RoomModel> _roomModels;
+
+        public RoomAdapter(List<RoomModel> roomModels)
+        {
+            _roomModels = roomModels;
+        }
+
+        public override int ItemCount
+        {
+            get { return ((List<RoomModel>)_roomModels).Count; }
+        }
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            RoomViewHolder viewHolder = holder as RoomViewHolder;
+            var roomType = _roomModels[position].RoomType;
+            switch (roomType)
+            {
+                case Constants.RoomType.Academics:
+                    viewHolder.ImageView.SetImageResource(Resource.Drawable.academics);
+                    break;
+                case Constants.RoomType.Games:
+                    viewHolder.ImageView.SetImageResource(Resource.Drawable.games);
+                    break;
+                case Constants.RoomType.Hangouts:
+                    viewHolder.ImageView.SetImageResource(Resource.Drawable.hangouts);
+                    break;
+                case Constants.RoomType.Sports:
+                    viewHolder.ImageView.SetImageResource(Resource.Drawable.sports);
+                    break;
+            }
+            viewHolder.RoomDescription.Text = _roomModels[position].RoomDescription;
+            viewHolder.RoomName.Text = _roomModels[position].RoomName;           
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).
+                     Inflate(Resource.Layout.room_recyclerview, parent, false);
+            RoomViewHolder viewHolder = new RoomViewHolder(itemView);
+            return viewHolder;
+        }
     }
 }
--- Models/RoomModel.cs
+++ Models/RoomModel.cs
@@ -1,8 +1,12 @@
-﻿namespace MP140.Models
+﻿using static MP140.Constants;
+
+namespace MP140.Models
 {
     public class RoomModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string RoomName { get; set; }
+        public string RoomDescription { get; set; }
+        public RoomType RoomType { get; set; }
     }
 }
