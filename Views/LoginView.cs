82c62bca7067fb93d2b9cfb655644297a385747c
Aleth Gillian <fernandez.aleth@gmail.com>
2021-06-06 15:25:31 +0800

    Added login interface, loginview

--- Views/LoginView.cs
+++ Views/LoginView.cs
@@ -1,22 +1,55 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using MP140.Interfaces;
 
 namespace MP140.Views
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class LoginView : AppCompatActivity
+    public class LoginView : AppCompatActivity, ILoginView
     {
+
+        private EditText et_username, et_password;
+        private Button btnLogin, btnRegister;
+        private ProgressBar progressBar;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);            
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_login);
             InitializeViews();
-        }        
+        }
+        public void DisplayProgressbar()
+        {
+            RunOnUiThread(() =>
+            {
+                progressBar.Visibility = Android.Views.ViewStates.Visible;
+            });
+        }
+        public void HideProgressBar()
+        {
+            RunOnUiThread(() =>
+            {
+                progressBar.Visibility = Android.Views.ViewStates.Invisible;
+            });
+        }
+        public void DisplayRegistrationPopup()
+        {
+            Dialog popupDialog = new Dialog(this);
+            popupDialog.SetContentView(Resource.Layout.popup_room);//Paltan mo tong layout
+            popupDialog.Window.SetSoftInputMode(SoftInput.AdjustResize);
+            popupDialog.Show();
+        }
         private void InitializeViews()
         {
-            
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+            et_username = FindViewById<EditText>(Resource.Id.et_username);
+            et_password = FindViewById<EditText>(Resource.Id.et_password);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+
         }
     }
 }
